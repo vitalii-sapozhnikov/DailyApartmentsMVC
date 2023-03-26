7968c5baa7611cac536f2ddc64b6010241df5859
Vitalii Sapozhnikov <vitalii.sapozhnikov@gmail.com>
2023-03-26 22:41:56 +0300

    Filter refactor

--- DailyApartmentsMVC/Controllers/HomeController.cs
+++ DailyApartmentsMVC/Controllers/HomeController.cs
@@ -17,23 +17,53 @@ namespace DailyApartmentsMVC.Controllers
             _context = context;
         }
 
-        public IActionResult Index(string country, string city, int? price, DateTime? startDate, DateTime? endDate, int? roomNumber, int? sleepingPlacesNumber)
+        public IActionResult Index(string country, 
+                                   string city, 
+                                   int? minPrice,
+                                   int? maxPrice,
+                                   string? daterange,
+                                   int? roomNumber, 
+                                   int? sleepingPlacesNumber)
         {
             var properties = _context.Properties.Include(a => a.PropertyOwner);
+
+            // daterange will contain the selected date range in the format "DD/MM/YYYY - DD/MM/YYYY"
+            DateTime? startDate, endDate;
+            if (!string.IsNullOrEmpty(daterange))
+            {
+                string[] dateRangeParts = daterange.Split(" - ");
+                startDate = DateTime.Parse(dateRangeParts[0]);
+                endDate = DateTime.Parse(dateRangeParts[1]);
+            }
 
             // Build the LINQ query to filter the properties.
             var filteredProperties = properties.Where(p =>
                 (string.IsNullOrEmpty(country) || p.Country == country) &&
                 (string.IsNullOrEmpty(city) || p.City == city) &&
-                (!price.HasValue || p.Price <= price) &&
+                (!minPrice.HasValue || p.Price >= minPrice) &&
+                (!maxPrice.HasValue || p.Price <= maxPrice) &&
                 (!roomNumber.HasValue || p.RoomNumber >= roomNumber) &&
                 (!sleepingPlacesNumber.HasValue || p.SleepingPlaceNumber >= sleepingPlacesNumber));
-
-            int numberOfSuggestions = filteredProperties.Count();
-            ViewBag.Message = $"Було знайдено ${numberOfSuggestions} пропозицій по вашому запиту!";
+                //(!startDate.HasValue || p. >= startDate) &&
+                //(!endDate.HasValue || p.EndDate <= endDate));
 
             return View(filteredProperties);
         }
+
+        #region AutoCompleteActions
+        public ActionResult CountryAutoComplete(string search)
+        {
+            var data = _context.Properties.GroupBy(p => p.Country).Select(grp => grp.First().Country);
+            var result = data.Where(x => x.ToLower().StartsWith(search.ToLower()));
+            return Json(result);
+        }
+        public ActionResult CityAutoComplete(string search)
+        {
+            var cities = _context.Properties.GroupBy(p => p.City).Select(grp => grp.First().City).ToList();
+            var filteredCities = cities.Where(c => c.ToLower().StartsWith(search.ToLower()));
+            return Json(filteredCities);
+        }
+        #endregion
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- DailyApartmentsMVC/wwwroot/js/site.js
+++ DailyApartmentsMVC/wwwroot/js/site.js
@@ -1,4 +1,76 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Auto-complete input for country
+$(function () {
+    $("#country").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "/Home/CountryAutoComplete",
+                dataType: "json",
+                data: {
+                    search: request.term
+                },
+                success: function (data) {
+                    response(data);
+                }
+            });
+        },
+        minLength: 3, // Minimum length of input before autocomplete starts
+        menu: '<ul class="ui-autocomplete ui-menu"></ul>',
+        open: function (event, ui) {
+            // Move the dropdown menu below the input field
+            var $menu = $(event.target).data("ui-autocomplete").menu.element;
+            $menu.position({
+                my: "left top",
+                at: "left bottom",
+                of: event.target
+            });
 
-// Write your JavaScript code.
+        }
+    });
+});
+
+// Auto-complete input for city
+$(function () {
+    $("#city").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "/Home/CityAutoComplete",
+                dataType: "json",
+                data: {
+                    search: request.term
+                },
+                success: function (data) {
+                    response(data);
+                }
+            });
+        },
+        minLength: 3, // Minimum length of input before autocomplete starts
+        menu: '<ul class="ui-autocomplete ui-menu"></ul>',
+        open: function (event, ui) {
+            // Move the dropdown menu below the input field
+            var $menu = $(event.target).data("ui-autocomplete").menu.element;
+            $menu.position({
+                my: "left top",
+                at: "left bottom",
+                of: event.target
+            });
+
+        }
+    });
+});
+
+
+$(function () {
+    $('#daterange').daterangepicker({
+        opens: 'left',
+        locale: {
+            format: 'DD/MM/YYYY'
+        },
+        minDate: moment()
+    }, function (start, end, label) {
+        //console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+});
+
+
+
+
